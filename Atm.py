c6a3df1f3d76d8c11630a25ef51cb56e14a43e37
Xenon-Game-Dev <82573055+Xenon-Game-Dev@users.noreply.github.com>
2021-08-12 18:57:39 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,13 @@
+class atm :
+    def __init__(self, cardNo, pinNo):
+        self.cardNo = cardNo
+        self.pinNo = pinNo
+    def cashWithdrawl():
+        print("Take some money")
+    def balanceEnquiry():
+        print("You are $10,000 in debt. Have fun with capitalism")        
+
+ATM = atm(298034572390, 2529)
+
+atm.cashWithdrawl()
+atm.balanceEnquiry()
